10d264da4af5500e559af9892fd973df80ef6323
acirom <114176829+acirom@users.noreply.github.com>
2022-09-26 18:50:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
+let books = [];
+
 const cors = require('cors');
 app.use(cors());
 
@@ -10,11 +12,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello from the server acic test!');
 });
 
 app.get('/health', (req, res) => {
   res.sendStatus(200);
+});
+
+app.post('/book', (req, res) => {
+  const book = req.body;
+  books.push(book);
+  //console.log(book);
+  res.json(book);
+  //res.send('Book was added to the database');
+  // res.json({
+  //   number: 1
+  // });
+  //res.send('{"success" : "Updated Successfully"' & book.id & ', "status" : 200}');
+
+});
+
+app.get('/books', (req, res) => {
+  res.json(books);
 });
 
 app.listen(port, () => {
