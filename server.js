a95e045a1debf8df32d886ba895a93beac5d0925
acirom <114176829+acirom@users.noreply.github.com>
2022-09-26 18:38:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
